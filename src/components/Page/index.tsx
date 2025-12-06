7c08ee34bcb65ca99b04a9fa1f189af3b404ed4b
Aomariayman <aymanaomaripro@gmail.Com>
2025-12-06 16:14:46 +0100

    add the babel for a runtime env

--- src/components/Page/index.tsx
+++ src/components/Page/index.tsx
@@ -4,8 +4,8 @@ import type { PageProps } from "./types.js";
 const Page: React.FC<PageProps> = ({ title, children }) => {
   return (
     <>
-      <h1>{title} - this is my text</h1>
-      {children}
+      <h1>{title}</h1>
+      <>{children}</>
     </>
   );
 };
